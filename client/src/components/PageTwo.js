7433ca16cd8854a313c3d047933bc8710c984c0a
KeshavRao-exe <73833127+KeshavRao-exe@users.noreply.github.com>
2022-03-17 11:59:36 +0530

    Page Two alignment

--- client/src/components/PageTwo.js
+++ client/src/components/PageTwo.js
@@ -11,11 +11,8 @@ export default function PageTwo(){
                     <li>Step 2 kjgyjrdjyfukhb yjgj yhfc kyvluhvygk  hjb kuftyf kytv kluv u it d6rd6rt yugyu fiotf  uiugo67trfi67f ou pyup87g</li>
                     <li>Step 3 gcrestg il iyu gvyit glu buyotyfio uly ouvo7tyfri6ditf uo tyf7t y hi oyugi6ry d</li>
                 </ul>
-                
+                <button class="verifybutton">Verify Status</button>
                 </div>
-                <button class="bg-green-400 hover:bg-gray-100 text-gray-800 font-bold font-semibold py-2 px-4 border border-gray-400 rounded verify-btn">
-                Verify Status
-                </button>
             </div>
             
 </div>
