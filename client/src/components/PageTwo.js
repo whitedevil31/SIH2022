6b1d770a6010a92ba648201c85c7c6ee62269f98
codebuilder2022 <kumarankaushik@gmail.com>
2022-03-17 12:03:48 +0530

    ✨

--- client/src/components/PageTwo.js
+++ client/src/components/PageTwo.js
@@ -1,61 +1,71 @@
-import React, {useState} from 'react'
-export default function PageTwo(){
-    const [modal, setModal] = useState(false);
-  
-    const toggleModal = () => {
-      setModal(!modal);
-    };
-  
-    if(modal) {
-      document.body.classList.add('active-modal')
-    } else {
-      document.body.classList.remove('active-modal')
-    }
-    return(
+import React, { useState } from "react";
+import Modal from "react-modal";
 
-    
-        <div className='pageTwo' >
-            <div className="bg-img">
-                <div className="logo2">/eft Shift</div>
-                <div className="qr-boc"></div>
-                <div className="inst">
-                <ul className='steps'>
-                    <li>Step 1: Please download out app from </li>
-                    <li>Step 2 kjgyjrdjyfukhb yjgj yhfc kyvluhvygk  hjb kuftyf kytv kluv u it d6rd6rt yugyu fiotf  uiugo67trfi67f ou pyup87g</li>
-                    <li>Step 3 gcrestg il iyu gvyit glu buyotyfio uly ouvo7tyfri6ditf uo tyf7t y hi oyugi6ry d</li>
-                </ul>
-                
-                </div>
-                {/* <button onClick={toggleModal} class="bg-green-400 hover:bg-gray-100 text-gray-800 font-bold font-semibold py-2 px-4 border border-gray-400 rounded verify-btn">
-                Verify Status
-                </button> */}
+export default function PageTwo() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
 
-<button onClick={toggleModal} className="btn-modal">
-        Open
-      </button>
+  let subtitle;
 
-      {modal && (
-        <div className="modal">
-          <div onClick={toggleModal} className="overlay"></div>
-          <div className="modal-content">
-            <h2>Hello Modal</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-              perferendis suscipit officia recusandae, eveniet quaerat assumenda
-              id fugit, dignissimos maxime non natus placeat illo iusto!
-              Sapiente dolorum id maiores dolores? Illum pariatur possimus
-              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
-              placeat tempora vitae enim incidunt porro fuga ea.
-            </p>
-            <button className="close-modal" onClick={toggleModal}>
-              CLOSE
-            </button>
-          </div>
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  return (
+    <div className="pageTwo">
+      <div className="bg-img">
+        <div className="logo2">/eft Shift</div>
+        <div className="qr-boc"></div>
+        <div className="inst">
+          <ul className="steps">
+            <li>Step 1: Please download out app from </li>
+            <li>
+              Step 2 kjgyjrdjyfukhb yjgj yhfc kyvluhvygk hjb kuftyf kytv kluv u
+              it d6rd6rt yugyu fiotf uiugo67trfi67f ou pyup87g
+            </li>
+            <li>
+              Step 3 gcrestg il iyu gvyit glu buyotyfio uly ouvo7tyfri6ditf uo
+              tyf7t y hi oyugi6ry d
+            </li>
+          </ul>
         </div>
-      )}
-      <p>pisci eaque laboriosam nam quaerat voluptate quae voluptatibus explicabo quas retis assumenda ugit repellendus? Illum maxime obcaecati, animi tenetur eos totam. Sint iure omnis velit as</p>
-            </div>
-            
-</div>
-    )
+        <button
+          onClick={openModal}
+          class="bg-green-400 hover:bg-gray-100 text-gray-800 font-bold font-semibold py-2 px-4 border border-gray-400 rounded verify-btn"
+        >
+          Verify Status
+        </button>
+
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+        </Modal>
+      </div>
+    </div>
+  );
 }
