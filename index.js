e919948ec0c75d9a05fb2a174d28fe12c1e1a07b
whitedevil31 <magz3116@gmail.com>
2022-03-17 13:36:19 +0530

    Add register route

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express();
 const cors = require("cors");
 dotenv.config({ path: "./.env" });
 const connectDB = require("./src/db");
+const userRoute = require("./src/routes/userRoutes");
 connectDB();
 
 app.use(express.json())
 app.use(cors());
-port = process.env.PORT || 3000;
+app.use('/user',userRoute);
+port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log("server is up on port " + port);
 });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const User = require("../model/userSchema");
+const User = require("../models/userSchema");
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 
-
-router.post('/',(req,res)=>{
+router.post('/register',async(req,res)=>{
   try{
-    res.json("hello")
+   const mobileNumber = req.body.mobileNumber;
+   const saltRounds = 12;
+   const salt = await bcrypt.genSalt(saltRounds);
+   const mobileHash = await bcrypt.hash(mobileNumber, salt);
+const registerUser = new User({mobileNumber:mobileHash})
+await registerUser.save()
+res.status(200).json({message:"User registered"})
   }catch(err)
   {
-
+console.log(err)
+res.json(400).json("Something went wrong ")
   }
 })
 
